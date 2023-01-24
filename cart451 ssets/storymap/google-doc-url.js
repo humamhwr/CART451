44b82a02769debba87f75ec46792d4fa43c3bf58
Humam Hawara <himoo1999@gmail.com>
2023-01-24 10:11:17 -0500

    push

---
+++ cart451 ssets/storymap/google-doc-url.js
@@ -0,0 +1,5 @@
+//google sheets
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/18vDYVbiweirOSCTNfwl20X-K7xSwyeApWeQkv0DoXpc/edit#gid=0';
+
+//google api
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
